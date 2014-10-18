cf06262e1b383b824057a7d7e6baed5ca3038910
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-17 23:18:27 -0500

    created empty datetime bolt

---
+++ odin/bolts/datetime.php
@@ -0,0 +1,10 @@
+<?php
+#CreatedBy;Aaron;17OCT2014;Odin-Framework
+class bolt_datetime
+{
+	#This will be a wrapper for developers to use the native php datetime class.
+	function __construct()
+	{
+		
+	}
+}
