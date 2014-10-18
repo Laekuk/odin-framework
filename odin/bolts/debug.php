9b8ae18181541066d2b8dac71156841d8e1edd95
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-17 23:15:47 -0500

    added sassy error

--- odin/bolts/debug.php
+++ odin/bolts/debug.php
@@ -20,10 +20,12 @@ class bolt_debug
 /*	This is intended to be called internally for dynamic-based errors,
 	meaning they are errors that are not just caused by the developer setting stuff up wrong.
 	they're for when your MySQL server is down, or an API your application is depending on has gone missing.
-*/	function error($msg,$force_error=NULL)
+*/	function error($msg=NULL,$force_error=NULL)
 	{
 		if($this->is_developer() || $force_error || $this->force_error)
 		{
+			if(empty($msg))
+				{ $msg	= htmlspecialchars('<tone sass="on">No Message Passed..</tone>'); }
 			#You are a developer, or this error should display to the general public (meaning EVERYONE).
 			echo "<h1>Error</h1>";
 			if(is_string($msg))
@@ -36,10 +38,11 @@ class bolt_debug
 			}
 			echo "<h1>Developer Backtrace</h1>";
 			echo '<pre style="background:#222;border:2px dotted #000;margin:10px;padding:15px;text-align:left;-moz-border-radius:10px;color:#009047;overflow:auto;opacity:75;font-size:11px;">';
-			$debug			= debug_backtrace();
-			$total_lvls		= count($debug);
-			$dbg_i			= 0;
-			$new_backtrace	= array();
+			$debug					= debug_backtrace();
+			$debug[0]["args"][0]	= "<em>See Above..</em>";
+			$total_lvls				= count($debug);
+			$dbg_i					= 0;
+			$new_backtrace			= array();
 			while(($dbg_i<$this->backtrace_levels || $this->backtrace_levels===0) && !empty($debug))
 			{
 				$dbg_i++;
