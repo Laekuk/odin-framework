8e02ba36ed78760865a9d9f12b55c9a13c4e1341
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-19 17:28:47 -0500

    Reformatted code to match rest of the framework

--- odin/bolts/str.php
+++ odin/bolts/str.php
@@ -5,14 +5,19 @@ class bolt_str
 	// strip all non alpha numeric characters
 	function alpha_num($str,$replace=" ")
 	{
-		$empty = false; $space = false;
-		if(empty($replace)) { $replace = '_'; $empty=true; }
-		if($replace == " ") { $replace = '_'; $space=true; }
-		$replace = ($empty?"":$replace);
-		$str = trim(preg_replace(array('/[^0-9a-z]+/i','/\s+/'),' ',$str));
-		if($space)			{ $str = str_replace(' ',$replace,$str); }
+		$empty		= FALSE;
+		$space		= FALSE;
+		if(empty($replace))
+			{ $replace = '_'; $empty=true; }
+		if($replace == " ")
+			{ $replace = '_'; $space=true; }
+		$replace	= ($empty?"":$replace);
+		$str		= trim(preg_replace(array('/[^0-9a-z]+/i','/\s+/'),' ',$str));
+		if($space)
+			{ $str	= str_replace(' ',$replace,$str); }
 		return $str;
 	}
+
 	// make string lower case with no alpha numeric characters
 	function tokey($str)
 		{ return strtolower($this->alpha_num($str,"_")); }
