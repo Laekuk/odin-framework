f9b9d929d9397078b6224cd28d780c566cb3790e
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-27 16:24:54 -0500

    added bricks & apps to path configs

--- odin/_conf/odin.php
+++ odin/_conf/odin.php
@@ -8,7 +8,10 @@ class conf_odin
 		$this->paths	= (object) array(
 			"base"			=> $my_dir."/",
 			"configs"		=> $my_dir."/_conf/",
-			'bolts'		 	=> $my_dir."/bolts/",
+			'bolts'		 	=> $my_dir."/bolts/",		#A single classes of code
+			'bricks'	 	=> $my_dir."/bricks/",		#folders of code. usually using multiple bolts, adding their own functionality.
+			'apps'		 	=> $my_dir."/apps/",		#The tenative name for groups of bricks.
+														#example: forum software, which is using a users brick, as well as a content manipulation brick
 		);
 
 	}
