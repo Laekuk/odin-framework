fce9ea5955c62ba25194b5d0a84996df1dabcaeb
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-27 17:33:07 -0500

    updated config copy

--- odin/bricks/_freyja/conf.php
+++ odin/bricks/_freyja/conf.php
@@ -1,2 +1,2 @@
 <?php
-$conf	= array("p"=>"Freyja rules over her heavenly afterlife field Fólkvangr and there receives half of those that die in battle, whereas the other half go to the god Odin's hall, Valhalla.");
+$conf	= array("p"=>"Freyja rules over her heavenly afterlife field Fólkvangr and there receives half of those that die in battle, whereas the other half go to the god Odin's hall, Valhalla.<hr />Freyja is associated with:");
