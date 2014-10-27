2203bc0b0d2e6fd7075fa94cecd57d098ac05ecd
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-27 17:28:39 -0500

    initial commit of the _freyja brick (example code for how bricks work)

---
+++ odin/bricks/_freyja/conf.php
@@ -0,0 +1,2 @@
+<?php
+$conf	= array("p"=>"Freyja rules over her heavenly afterlife field Fólkvangr and there receives half of those that die in battle, whereas the other half go to the god Odin's hall, Valhalla.");
