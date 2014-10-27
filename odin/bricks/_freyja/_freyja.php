411d31fe4641c5838dea2d843b4c828b28e8d120
Aaron <admin@Aaron-Macbook-Pro.local>
2014-10-27 17:28:35 -0500

    initial commit of the _freyja brick (example code for how bricks work)

---
+++ odin/bricks/_freyja/_freyja.php
@@ -0,0 +1,21 @@
+<?php
+class brick__freyja extends brick_mortar
+{
+	var $config;
+	var $my_path;
+	function __construct($conf)
+	{
+		$this->config	= $conf;
+		$this->set_brick_path(dirname(__FILE__)."/",str_replace("brick_",NULL,__CLASS__));
+	}
+
+	function TOIF($saying=FALSE)
+	{
+		if(!$saying)
+			{ $saying	= "Thank Odin, its Freyja!"; }
+		$saying	= "<h2>".$saying."</h2>"
+			."<p>".$this->config["p"]."</p>"
+			.$this->features->list_them();
+		return $saying;
+	}
+}
