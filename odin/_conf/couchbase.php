c85800b3bf6dbe18c462e243e3ea8c16b15367b2
Aaron <admin@Aaron-Macbook-Pro.local>
2014-11-01 21:38:14 -0500

    initial push of couchbase config

---
+++ odin/_conf/couchbase.php
@@ -0,0 +1,7 @@
+<?php
+#CreatedBy;Aaron;01NOV2014;Odin-Framework
+$conf	= array(
+	'default_host'	=> NULL,#Hostname or IP of your couchbase server
+	'auth'			=> NULL,#Your login credentials. example: array('user'=>'Administrator','pass'=>'foo')
+	'default_port'	=> NULL,#system assumes default port of 8092 if this is left blank
+);
