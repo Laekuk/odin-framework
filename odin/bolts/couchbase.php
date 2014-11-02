b025aa8f0c0185260009440592e4d41555d966be
Aaron <admin@Aaron-Macbook-Pro.local>
2014-11-01 22:27:45 -0500

    changed to use $opts, instead of just a $debug flag. Also added the first parameter "limit"

--- odin/bolts/couchbase.php
+++ odin/bolts/couchbase.php
@@ -21,19 +21,27 @@ class bolt_couchbase
 		$this->login	= $login['user'].':'.$login['pass'];
 	}
 
-	function pull($bucket,$view_path,$debug=NULL)
+	#See http://docs.couchbase.com/couchbase-manual-2.5/cb-rest-api/#querying-views-with-rest for full spec
+	function pull($bucket,$view_path,$opts=NULL)
 	{
 		global $odin;
+		#if they exist, merge the passed options with the defaults.
+		$o	= array(
+			'debug'	=> NULL,	#If debug is switched to true, it will return information about the cURL request instead of just returning data.
+			'limit'	=> NULL,	#Limits the number of results recieved
+		);
+		if($opts)
+			{ $o	= $odin->array->ow_merge_r($o,$opts); }
 		if(!$this->server)
 			{ return $odin->debug->error('No host set'); }
-		$uri	= $this->server.'/'.$bucket.'/'.$view_path;
-		$o		= array(
+		$uri	= $this->server.'/'.$bucket.'/'.$view_path.($o['limit']?'?limit='.$o['limit']:'');
+		$curl_o		= array(
 			CURLOPT_USERPWD 		=> $this->login,
-			CURLOPT_CONNECTTIMEOUT	=> 3,
+			CURLOPT_CONNECTTIMEOUT	=> 4,
 		);
-		if($debug)
+		if($o['debug'])
 			{ $o['return_all']	= 1; }
-		$ret	= $odin->curl->request($uri,$o);
+		$ret	= $odin->curl->request($uri,$curl_o);
 		return json_decode($ret);
 	}
 }
