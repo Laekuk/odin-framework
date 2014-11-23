3b4e82ac3ccf647018de9b3e6e513d167af125cd
Aaron <admin@Aaron-Macbook-Pro.local>
2014-11-23 01:15:48 -0600

    added ‘id’ to single’s options to make it an update form, rather than an insert form

--- odin/bolts/crud.php
+++ odin/bolts/crud.php
@@ -15,96 +15,147 @@ class bolt_crud
 		$this->num_instances++;
 		#loose cleanse against possible injections.
 		$table	= $odin->str->alpha_num($table);
-		$instance	= (isset($o["instance"])?$o["instance"]:"crud-inst-".$this->num_instances);
+		$instance	= (isset($o['instance'])?$o['instance']:'crud-inst-'.$this->num_instances);
 		$o	= array(
-			"form_labels"	=> array("Add"),	#the form's label
-			"supress_cols"	=> NULL,			#an array of columns to hide on the insert form
-			"locked_vals"	=> NULL,			#an array of columns & their forced values. This also hides the columns by default (overwritten by col_types)
-			"defaults"		=> NULL,			#an array of columns & their default values, which overwrites the database-default-guesses
-			"col_types"		=> NULL,			#an array of column types, which overwrite the database-default-guesses
-			"col_options"	=> NULL,			#an array of column options, which overwrites the database-default-guesses
-			"col_rules"		=> NULL,			#set to overwrite the database-default-guesses for validation rules. Acceptable formats are:
-/*				This will validate if the posted value is numeric, if it is not it will return the error message "Must be a Number"
-				array("field"=>array("function_name"	=> "is_numeric",	"error"	=> "Must be A Number"))
+			'form_labels'		=> array('Add'),	#the form's label
+			'submit_text'		=> 'Add',			#the copy that goes on the submit button
+			'beautify_headings'	=> NULL,			#a boolean to determine if we'll beautify the headings
+			'supress_cols'		=> NULL,			#an array of columns to hide on the insert form
+			'locked_vals'		=> NULL,			#an array of columns & their forced values. This also hides the columns by default (overwritten by col_types)
+			'defaults'			=> NULL,			#an array of columns & their default values, which overwrites the database-default-guesses
+			'id'				=> NULL,			#a primary key of a row you want to edit. If left blank, this form will be an insert, rather than an update
+			'col_types'			=> NULL,			#an array of column types, which overwrite the database-default-guesses
+			'col_options'		=> NULL,			#an array of column options, which overwrites the database-default-guesses
+			'col_rules'			=> NULL,			#set to overwrite the database-default-guesses for validation rules. Acceptable formats are:
+/*				This will validate if the posted value is numeric, if it is not it will return the error message 'Must be a Number'
+				array('field'=>array('function_name'	=> 'is_numeric',	'error'	=> 'Must be A Number'))
 
 				This will run the alpha_num() method inside of the str bolt (passing the posted value to that method as the first paramiter),
 					then replace whatever was posted with the alpha_num
 				array(
-					"field"=>array(
-						"bolt"					=> "str",
-						"method"				=> "alpha_num",
-						"use_returned_value"	=> TRUE,
+					'field'=>array(
+						'bolt'					=> 'str',
+						'method'				=> 'alpha_num',
+						'use_returned_value'	=> TRUE,
 					)
 				)
 
 				This will validate if the posted value is numeric, if it is not it will set the posted value to an int of 0
-				array("field"=>array("function_name"	=> "is_numeric",	"fix_value"	=> 0))
+				array('field'=>array('function_name'	=> 'is_numeric',	'fix_value'	=> 0))
 
-				This will overwrite your posted data's value with "Tested"
-				array("field"=>array("set"				=> "Tested"))
+				This will overwrite your posted data's value with 'Tested'
+				array('field'=>array('set'				=> 'Tested'))
 */
-			"instance"		=> $instance,		#set to manually create the id for this form.
-			"headings"		=> NULL,			#set to overwrite the database-default-guesses for column headings
-			"new_set_on"	=> NULL,			#an array of fields to break the fieldsets up on.
-			"foreign_keys"	=> NULL,			#an array("field"=>array("sql"=>"SELECT * FROM `other_table` WHERE `field`>$value",array(":field"=>5))
+			'instance'		=> $instance,		#set to manually create the id for this form.
+			'headings'		=> NULL,			#set to overwrite the database-default-guesses for column headings
+			'new_set_on'	=> NULL,			#an array of fields to break the fieldsets up on.
+			'foreign_keys'	=> NULL,			#an array('field'=>array('sql'=>"SELECT * FROM `other_table` WHERE `field`>$value",array(':field'=>5))
 				#Pulls data from that other_table and loads it into this field's options.
-			"hide_pk"		=> TRUE,		#disable to display the primary key as read-only
+			'hide_pk'		=> TRUE,		#disable to display the primary key as read-only
 		);
 		if($opts)
 			{ $o	= $odin->array->ow_merge_r($o,$opts); }
 		$sql	= "DESCRIBE `$table`";
-		$fields	= $odin->sql->qry($sql,NULL,"Field");
+		$fields	= $odin->sql->qry($sql,NULL,'Field');
 		$fields	= $this->parse_fields($fields);
-		if($o["defaults"])
-			{ $fields["fields"]	= $odin->array->ow_merge_r($fields["fields"],$o["defaults"]); }
-		if($o["col_types"])
-			{ $fields["types"]	= $odin->array->ow_merge_r($fields["types"],$o["col_types"]); }
-		if($o["headings"])
-			{ $fields["types"]	= $odin->array->ow_merge_r($fields["headings"],$o["headings"]); }
-		if($o["col_options"])
-			{ $fields["options"]	= $odin->array->ow_merge_r($fields["options"],$o["col_options"]); }
+		if($o['defaults'])
+			{ $fields['fields']	= $odin->array->ow_merge_r($fields['fields'],$o['defaults']); }
+		if($o['col_types'])
+			{ $fields['types']	= $odin->array->ow_merge_r($fields['types'],$o['col_types']); }
+		if($o['headings'])
+			{ $fields['types']	= $odin->array->ow_merge_r($fields['headings'],$o['headings']); }
+		if($o['col_options'])
+			{ $fields['options']	= $odin->array->ow_merge_r($fields['options'],$o['col_options']); }
 
-		if($o["col_rules"])
-			{ $fields["rules"]	= $odin->array->ow_merge_r($fields["rules"],$o["col_rules"]); }
+		if($o['col_rules'])
+			{ $fields['rules']	= $odin->array->ow_merge_r($fields['rules'],$o['col_rules']); }
 		
 		if(isset($_REQUEST[$instance]))
 		{
 			$data	= $_REQUEST[$instance];
 			$info	= $this->validate_data($data,$fields);
-			if(empty($info["errors"]))
+			if(empty($info['errors']))
 			{
-				#insert into database.
-				$insert	= $odin->qdb->insert($table,$info["data"]);
-				if(!$insert)
-					{ $info["errors"]["_general"]	= "Unable to complete action."; }
-				else#throw a general form error.
+				$error_hit	= NULL;
+				#Insert?
+				if(!$opts['id'])
 				{
-					#clear post.
-					unset($_POST[$instance]);
+					#insert into database.
+					$insert	= $odin->qdb->insert($table,$info['data']);
+					if(!$insert)
+						{ $error_hit	= TRUE; }
+					else
+					{
+						#set success message
+						$info['msg']['_general']	= 'Successfully Added';
+					}
 				}
+				else#Update
+				{
+					#update database field.
+
+					#prevent html modification attempts
+					if($info['data'][$fields['primary']]!==$opts['id'])
+						{ $info['data'][$fields['primary']]	= $opts['id']; }
+					$update	= $odin->qdb->update($table,$info['data'],$fields['primary']);
+					if(!$update)
+						{ $error_hit	= TRUE; }
+					else
+					{
+						#set success message
+						$info['msg']['_general']	= 'Successfully Updated';
+					}
+				}
+				#clear post.
+				unset($_POST[$instance]);
+				#throw a general form error?
+				if($error_hit)
+					{ $info['errors']['_general']	= 'Unable to complete action.'; }
 			}
 			else
 			{
 				#interlace errors into form somehow
 			}
 		}
+		if($opts['id'])
+		{
+			#get this row's record
+			$sql		= "SELECT * FROM `$table` WHERE `$fields[primary]`=?";
+			$cur_values	= $odin->sql->qry($sql,array($opts['id']));
+			if(is_array($cur_values))
+			{
+				#update base copy on the form, if its still being used
+				if($o['form_labels'][0]=='Add')
+					{ $o['form_labels'][0]	= 'Edit'; }
+				if($o['submit_text']=='Add')
+					{ $o['submit_text']	= 'Edit'; }
+				$cur_values	= current($cur_values);
+				#merge this row with the defaults
+				$fields['fields']	= $odin->array->ow_merge_r($fields['fields'],$cur_values);
+			}
+			else
+			{
+				#error, no record to update.
+				return '<p class="error">Error, record lookup failed</p>';
+			}
+		}
 		$hide_fields	= array();
-		if($o["supress_cols"])
-			{ $hide_fields	= array_merge($hide_fields,$o["supress_cols"]); }
-		if($o["hide_pk"])
-			{ $hide_fields	= array_merge($hide_fields,array($fields["primary"])); }
+		if($o['supress_cols'])
+			{ $hide_fields	= array_merge($hide_fields,$o['supress_cols']); }
+		if($o['hide_pk'])
+			{ $hide_fields	= array_merge($hide_fields,array($fields['primary'])); }
 		$form_opts	= array(
-			"instance"		=> $o["instance"],
-			"submit_text"	=> "Add",
-			"legends"		=> $o["form_labels"],
-			"new_set_on"	=> $o["new_set_on"],
-			"instance"		=> $o["instance"],
-			"field_types"	=> $fields["types"],
-			"field_opts"	=> $fields["options"],
-			"hide_fields"	=> $hide_fields,
+			'instance'		=> $o['instance'],
+			'submit_text'	=> $o['submit_text'],
+			'legends'		=> $o['form_labels'],
+			'new_set_on'	=> $o['new_set_on'],
+			'instance'		=> $o['instance'],
+			'field_types'	=> $fields['types'],
+			'field_opts'	=> $fields['options'],
+			'hide_fields'	=> $hide_fields,
 		);
 
-		$form	= $odin->html->form($fields["fields"],$form_opts);
+		$form	= $odin->html->form($fields['fields'],$form_opts);
 		
 		return $form;
 	}
@@ -113,23 +164,23 @@ class bolt_crud
 	function multi($table,$opts=NULL)
 	{
 		$this->num_instances++;
-		$instance	= (isset($o["instance"])?$o["instance"]:"crud-inst-".$this->num_instances);
+		$instance	= (isset($o['instance'])?$o['instance']:'crud-inst-'.$this->num_instances);
 		$o	= array(
-			"form_label"	=> "Data",		#the form's label
-			"supress_cols"	=> NULL,		#an array of columns to hide on the table (not hidden on the edit-form)
-			"locked_vals"	=> NULL,		#an array of columns & their forced values. This also hides the columns by default (overwritten by col_types)
-			"col_types"		=> NULL,		#an array of column types, which overwrite the database-default-guesses
-				#For options, se an array("field"=>array("type"=>"select","options"=>array("key"=>"value")))
-			"col_rules"		=> NULL,		#set to overwrite the database-default-guesses for validation rules
-			"instance"		=> $instance,	#set to manually create the id for this form.
-			"headings"		=> NULL,		#set to overwrite the database-default-guesses for column headings
-			"foreign_keys"	=> NULL,		#an array("field"=>array("sql"=>"SELECT * FROM `other_table` WHERE `field`>$value",array(":field"=>5))
+			'form_label'	=> 'Data',		#the form's label
+			'supress_cols'	=> NULL,		#an array of columns to hide on the table (not hidden on the edit-form)
+			'locked_vals'	=> NULL,		#an array of columns & their forced values. This also hides the columns by default (overwritten by col_types)
+			'col_types'		=> NULL,		#an array of column types, which overwrite the database-default-guesses
+				#For options, se an array('field'=>array('type'=>'select','options'=>array('key'=>'value')))
+			'col_rules'		=> NULL,		#set to overwrite the database-default-guesses for validation rules
+			'instance'		=> $instance,	#set to manually create the id for this form.
+			'headings'		=> NULL,		#set to overwrite the database-default-guesses for column headings
+			'foreign_keys'	=> NULL,		#an array('field'=>array('sql'=>"SELECT * FROM `other_table` WHERE `field`>$value",array(':field'=>5))
 				#Pulls data from that other_table and loads it into this field's options.
-			"edit"			=> TRUE,		#enables the page to be reloaded as an edit with a $_GET
-			"delete"		=> NULL,		#set to true to allow the user to delete rows
-			"hide_pk"		=> TRUE,		#disable to display the primary key as read-only
-			"order_by"		=> NULL,		#string to force a sort-order of the table
-			"extra_fields"	=> NULL,		#an array of extra fields you might want to throw in
+			'edit'			=> TRUE,		#enables the page to be reloaded as an edit with a $_GET
+			'delete'		=> NULL,		#set to true to allow the user to delete rows
+			'hide_pk'		=> TRUE,		#disable to display the primary key as read-only
+			'order_by'		=> NULL,		#string to force a sort-order of the table
+			'extra_fields'	=> NULL,		#an array of extra fields you might want to throw in
 		);
 	}
 	
@@ -147,31 +198,31 @@ class bolt_crud
 		{
 			$field[$k]		= NULL;
 			$heading[$k]	= ucwords($odin->str->alpha_num($k));
-			if($v["Key"]=="PRI")
+			if($v['Key']=='PRI')
 				{ $primary	= $k; }
 			#parse the field's data-type & information.
-			$field_type	= preg_split('/\(/', $v["Type"],2);
+			$field_type	= preg_split('/\(/', $v['Type'],2);
 			
-			$field_info	= (isset($field_type[1])?preg_split("/\,/", substr($field_type[1], 0,-1)):NULL);
+			$field_info	= (isset($field_type[1])?preg_split('/\,/', substr($field_type[1], 0,-1)):NULL);
 			$field_type	= $field_type[0];
 			switch($field_type)
 			{
 				default:
 				break;
-				case "int";
+				case 'int';
 					$field_info	= $field_info[0];
 					#treat this like a boolean by default.
 					if($field_info>1)
-						{ $type[$k]	= "number"; }
+						{ $type[$k]	= 'number'; }
 					else
-						{ $type[$k]	= "checkbox"; }
+						{ $type[$k]	= 'checkbox'; }
 					#force an integer type
 					$rules[$k]	= array(
-						array("callback"=>"is_numeric","fix_value"=>0),
+						array('callback'=>'is_numeric','fix_value'=>0),
 					);
 				break;
-				case "enum":
-					$type[$k]		= "select";
+				case 'enum':
+					$type[$k]		= 'select';
 					$field_opts	= NULL;
 					foreach($field_info as $info_key=>$info_val)
 					{
@@ -180,21 +231,21 @@ class bolt_crud
 					}
 					$options[$k]	= $field_opts;
 				break;
-				case "text":
-				case "longtext":
-					$type[$k]		= "textarea";
+				case 'text':
+				case 'longtext':
+					$type[$k]		= 'textarea';
 				break;
 			}
-			if($v["Default"])
-				{ $field[$k]	= $v["Default"]; }
+			if($v['Default'])
+				{ $field[$k]	= $v['Default']; }
 		}
 		return array(
-			"primary"	=> $primary,
-			"fields"	=> $field,
-			"types"		=> $type,
-			"headings"	=> $heading,
-			"options"	=> $options,
-			"rules"		=> $rules,
+			'primary'	=> $primary,
+			'fields'	=> $field,
+			'types'		=> $type,
+			'headings'	=> $heading,
+			'options'	=> $options,
+			'rules'		=> $rules,
 		);
 	}
 	
@@ -203,28 +254,28 @@ class bolt_crud
 		global $odin;
 		//!Fix Checkboxes
 		#find all checkbox fields, throw them into the $checkboxes array
-		$checkboxes	= array_keys($field_info["types"],"checkbox");
+		$checkboxes	= array_keys($field_info['types'],'checkbox');
 		if($checkboxes)
 		{
 			#Alter the posted fields the default values to be blank
 			foreach($checkboxes as $field)
 			{
 				if(!isset($data[$field]))
-					{ $field_info["fields"][$field]	= ""; }
+					{ $field_info['fields'][$field]	= ''; }
 			}
 			#merge back in the original fields, overwritten by the passed $data. This fixes when checkboxes are not checked on submit
 			#doing it this way keeps their keys in their proper order, passed data values still passed, and unchecked checkboxes still have keys
-			$data	= $odin->array->ow_merge_r($field_info["fields"],$data);
+			$data	= $odin->array->ow_merge_r($field_info['fields'],$data);
 		}
 
 		//!Validate field options to make sure it was correctly passed
-		if(!empty($field_info["options"]))
+		if(!empty($field_info['options']))
 		{
-			foreach($field_info["options"] as $opt_field=>$options)
+			foreach($field_info['options'] as $opt_field=>$options)
 			{
 				$data_val	= $data[$field];
 				if(!isset($options[$data_val]))
-					{ $errors[$field][]	= "Invalid answer"; }
+					{ $errors[$field][]	= 'Invalid answer'; }
 			}
 		}
 
@@ -232,19 +283,18 @@ class bolt_crud
 Valid Rule Options:
 	- set						: if set, this will be 
 	- skip						: if set to true, this field will be removed from data.
-	- "callback"				: either the name of a global function, or a bolt/method combo to call.
+	- 'callback'				: either the name of a global function, or a bolt/method combo to call.
 		- use_returned_value		: If this is set, replace the data value with whatever your function or method returned.
 		- flip_valid				: If this is set, reverse the outcome of $valid so true becomes false, false becomes true.
 		- invalid_skip				: If $valid is true, remove this field from the data.
 		- error						: If $valid is false, return this error string.
-		- fix_value					: If this is set & $valid is false, set the value to this. This overrides the "error" message from appearing, if both are set.
+		- fix_value					: If this is set & $valid is false, set the value to this. This overrides the 'error' message from appearing, if both are set.
 */
 		$errors	= array();
 		//!Run validation rules, recording all errors into $errors
-		if(!empty($field_info["rules"]))
+		if(!empty($field_info['rules']))
 		{
-var_dump($field_info["rules"]);
-			foreach($field_info["rules"] as $field=>$rules)
+			foreach($field_info['rules'] as $field=>$rules)
 			{
 				#loop through the rules array in order of their array position
 				$data_val	= $data[$field];
@@ -254,50 +304,50 @@ var_dump($field_info["rules"]);
 					switch(TRUE)
 					{
 						#set the posted value to whatever $rule[set] is
-						case isset($rule["set"]):
-							$data_val	= $rule["set"];
+						case isset($rule['set']):
+							$data_val	= $rule['set'];
 						break;
-						case isset($rule["skip"]):
+						case isset($rule['skip']):
 							$skip	= TRUE;
 						break;
-						case isset($rule["callback"]):
-							if(is_array($rule["callback"]))
+						case isset($rule['callback']):
+							if(is_array($rule['callback']))
 							{
-								$method	= array_pop($rule["callback"]);
-								if(method_exists($rule["callback"],$method))
-									{ $valid	= call_user_func_array(array($rule["callback"],$method), array($data_val)); }
+								$method	= array_pop($rule['callback']);
+								if(method_exists($rule['callback'],$method))
+									{ $valid	= call_user_func_array(array($rule['callback'],$method), array($data_val)); }
 							}
 							else
 							{
-								if(function_exists($rule["callback"]))
-									{ $valid	= $rule["callback"]($data_val); }
+								if(function_exists($rule['callback']))
+									{ $valid	= $rule['callback']($data_val); }
 							}
 							#run & possibly adjust the validation method.
 /*
 	$valid	= NULL;
 	if($is_function)	#this is a function, run it
-		{ $valid	= $rule["func"]($data_val); }
+		{ $valid	= $rule['func']($data_val); }
 	elseif($is_bolt)	#this must be a bolt/method, run it
-		{ $valid	= call_user_func_array(array($odin->{$rule["bolt"]},$rule["method"]), array($data_val)); }
+		{ $valid	= call_user_func_array(array($odin->{$rule['bolt']},$rule['method']), array($data_val)); }
 */
 							#flip the validation outcome?
-							if(isset($rule["flip_valid"]))
+							if(isset($rule['flip_valid']))
 								{ $valid	= !$valid; }
 
 							#should we skip this field?
-							if(!$valid && isset($rule["invalid_skip"]))
+							if(!$valid && isset($rule['invalid_skip']))
 								{ $skip	= TRUE; }
 
 							#handle failed validations
-							if(!$valid && (isset($rule["fix_value"]) || $rule["error"]))
+							if(!$valid && (isset($rule['fix_value']) || $rule['error']))
 							{
-								if(isset($rule["fix_value"]))
-									{ $data_val			= $rule["fix_value"]; }
+								if(isset($rule['fix_value']))
+									{ $data_val			= $rule['fix_value']; }
 								else
-									{ $errors[$field][]	= $rule["error"]; }
+									{ $errors[$field][]	= $rule['error']; }
 							}
 							#are we going to overwrite data with the validation value?
-							if(!empty($rule["use_returned_value"]))
+							if(!empty($rule['use_returned_value']))
 								{ $data_val	= $valid; }
 						break;
 					}
@@ -310,8 +360,8 @@ var_dump($field_info["rules"]);
 			}
 		}
 		return array(
-			"data"		=> $data,
-			"errors"	=> $errors,
+			'data'		=> $data,
+			'errors'	=> $errors,
 		);
 	}
 }
