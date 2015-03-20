b0ccb439c34e742db0f3f466055f87d50e48ebae
Aaron <admin@Aaron-Macbook-Pro.local>
2015-03-19 20:42:26 -0500

    added configurations for lowercase & uppercase words

---
+++ odin/_conf/str.php
@@ -0,0 +1,12 @@
+<?php
+#11OCT2014;AaronPeloquin;Odin-Framework
+$conf	= (object)[
+	'lowercase_words'	=> [
+		'a', 'an', 'the',
+		'and', 'but', 'or', 'for', 'nor',
+		'on', 'at', 'to', 'from', 'by',
+	],
+	'uppercase_words'	=> [
+		'id','pk','ip',
+	],
+];
