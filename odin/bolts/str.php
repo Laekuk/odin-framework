8990ac213b6575960e73b75b2cb631ff409313fa
Aaron <admin@Aaron-Macbook-Pro.local>
2015-03-19 19:29:02 -0500

    added string ‘beautify’ method

--- odin/bolts/str.php
+++ odin/bolts/str.php
@@ -2,6 +2,9 @@
 #CreatedBy;Weston;17OCT2014;Odin-Framework
 class bolt_str
 {
+	function __construct($conf)
+		{ $this->conf	= $conf; }
+
 	// strip all non alpha numeric characters
 	function alpha_num($str,$replace=" ")
 	{
@@ -21,4 +24,32 @@ class bolt_str
 	// make string lower case with no alpha numeric characters
 	function tokey($str)
 		{ return strtolower($this->alpha_num($str,"_")); }
+
+	function beautify($str)
+	{
+		if(is_array($str))
+		{
+			foreach($str as $k=>$v)
+				{ $str[$k]	= $this->beautify($v); }
+		}
+		elseif(is_string($str))
+		{
+			if(strpbrk($str, ' _')!==FALSE)
+			{
+				$str	= strtolower($str);
+				$str	= preg_split('/[\s_]/', $str);
+				foreach($str as &$word)
+				{
+					if(in_array($word, $this->conf->uppercase_words))
+						{ $word	= strtoupper($word); }
+					elseif(!in_array($word, $this->conf->lowercase_words))
+						{ $word	= ucfirst($word); }
+				}
+				$str	= implode(' ', $str);
+			}
+			else
+				{ $str	= ucfirst($str); }
+		}
+		return $str;
+	}
 }
